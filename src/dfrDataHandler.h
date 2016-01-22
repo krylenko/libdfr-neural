3ffaa550426e3ba7235b9d50ed194cb680375d86
krylenko <dan.f@bluerivert.com>
2016-01-21 22:09:47 -0800

    started data handler definition

---
+++ src/dfrDataHandler.h
@@ -0,0 +1,26 @@
+// data handler class
+// performs:
+//   -- parsing of input arguments: training data filename, net params, cross-validation params, type of task (MNIST, XOR, etc.)
+//   -- loading of training data
+//   -- timing training and inference
+//   -- displaying net params, status, and timing to the user
+//   -- data slicing, batching, and cross-validation
+//   -- feeding data into the net during training and inference
+//   -- net loading and saving
+//   -- possibly network setup based on task -- define a task w/ its params, don't specify manually
+
+// inputs: argc/argv (training data filename, net params, cross-validation params, type of task), net instance
+
+#ifndef __DFRDATAHANDLER_H__
+#define __DFRDATAHANDLER_H__
+
+class DataHandler
+{
+
+    public:
+
+    private:
+
+};
+
+#endif
